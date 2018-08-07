7b577cf838755a6f043c7770c6b1807c098ca262
Shiva1029 <Shiva1029@users.noreply.github.com>
2018-08-07 12:51:13 -0400

    Modernize
    Modernize

--- password.js
+++ password.js
@@ -1,11 +1,12 @@
-function generatePassword() {
-                var pwdGen = '';
-                var uCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-                var lCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-                for (var j = 0; j < 3; j++) {
-                    var rand = Math.floor((Math.random() * 10) + 17);
-                    pwdGen += rand.toString() + uCase[rand - 1] + lCase[rand - 1];
-                }
-                return pwdGen;
-            }
+const generatePassword = len => {
+  let pwdGen = '';
+  const charSet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '$', '@', '!', '/', '?', '-', ')',
+    '"', '#', '^', '*', '(', ']', '{', '}', '[', '%', '_', '+', '=', '~'
+  ];
+  const min = 0;
+  const max = charSet.length - 1;
+  for (let j = 0; j < len; j++) {
+    pwdGen += charSet[Math.floor((max - min + 1) * Math.random()) + min];
+  }
+  return pwdGen;
+}
